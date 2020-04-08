6359753b070f2a2f8e72f52428669fe23e214151
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-04-08 20:00:34 +0300

    lesson 1 task 4 completed

---
+++ les_1/task4/task_4-2/main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?=$title?></title>
+	<meta charset="UTF-8">
+</head>
+<body>
+<h1><?=$h1title?></h1>
+<?=$h1title?>
+<br><br>
+<?=$description?>
+<br><br>
+<?=$image?>
+<br><br>
+<b>Просто пример жирного текста</b>
+<br><br>
+<?=$date?>
+</body>
+</html>
---
+++ les_1/task4/task_4-3/index.php
@@ -0,0 +1,20 @@
+<?php
+$title = 'Главная страница - страница обо мне';
+$h1title = "Информация обо мне";
+$description = 'Краткая биография обо мне
+    Родился в 1985 году в городе Москва. Закончил в 2008 году МАИ.
+    На данный момент работаю ведущим инженером в крупной авиакомпании.
+    Поскольку я люблю авиацию, то хотел бы поделиться несколькими интересными
+    фотографиями на эту тему';
+$image = '<img src="../aviator.jpg" alt="">';
+$date = 2018;
+
+$content = file_get_contents("main.tmp");
+
+$content = str_replace("{{ TITLE }}", $title, $content);
+$content = str_replace("{{ H1-TITLE }}", $h1title, $content);
+$content = str_replace("{{ DESCRIPTION }}", $description, $content);
+$content = str_replace("{{ IMAGE }}", $image, $content);
+$content = str_replace("{{ DATE }}", $date, $content);
+
+echo $content;
