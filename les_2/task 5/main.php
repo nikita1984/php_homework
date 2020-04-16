827774346154d0acf05d4c2f5eeeaec3634ff381
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-04-16 12:23:39 +0300

    start commit task 2-5

---
+++ les_2/task 5/main.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<!-- файл шаблона для точки (файла) входа index.php -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?=$menu?>
+<?=$content?>
+</body>
+</html>
